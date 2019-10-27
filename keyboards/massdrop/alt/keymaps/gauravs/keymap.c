5327104476b06418aab2281af87eb82114600da9
Gaurav Sharma <gaurav.qwe@gmail.com>
2019-10-27 12:34:08 -0700

    updated from upstream

--- keyboards/massdrop/alt/keymaps/gauravs/keymap.c
+++ keyboards/massdrop/alt/keymaps/gauravs/keymap.c
@@ -40,10 +40,11 @@ enum {
 #define SECONDS *1000
 #define MINUTES *(60 SECONDS)
 
-#define LED_ANIMATION_ID_DEFAULT 5
-#define LED_ANIMATION_ID_LOWER   4
 #define LED_ANIMATION_ID_OFF     10
-#define LED_MODE_DEFAULT         LED_MODE_KEYS_ONLY
+#define LED_ANIMATION_ID_DEFAULT LED_ANIMATION_ID_OFF
+#define LED_ANIMATION_ID_LOWER   4
+// #define LED_MODE_DEFAULT         LED_MODE_KEYS_ONLY
+#define LED_MODE_DEFAULT         LED_MODE_NORMAL
 #define DFU_DURATION             1 SECONDS
 
 #ifdef HOME
@@ -61,24 +62,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        MO(1),      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
+        MO(1),      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_END, \
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   _______, \
         KC_LCTL,    KC_LGUI, KC_LALT,                            KC_SPC,                             MO(1),   KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [LOWER] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  MD_BOOT, KC_MUTE, \
-        L_T_BR,  L_PSD,   L_BRI,   L_PSI,   KC_TRNS, KC_TRNS, KC_TRNS, U_T_AUTO, U_T_AGCR, KC_HOME, KC_UP,   KC_END,  KC_PAUS, KC_TRNS, KC_END,  \
-        L_T_PTD, L_PTP,   L_BRD,   L_PTN,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_LEFT, KC_DOWN, KC_RGHT,          KC_TRNS, KC_VOLU, \
-        KC_TRNS, L_T_MD,  L_T_ONF, KC_TRNS, KC_TRNS, MD_BOOT, TG_NKRO, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,          KC_PGUP, KC_VOLD, \
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                              KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END   \
+        L_T_BR,  L_PSD,   L_BRI,   L_PSI,   _______, _______, _______, U_T_AUTO, U_T_AGCR, KC_HOME, KC_UP,   KC_END,  KC_PAUS, _______, KC_VOLU,  \
+        L_T_PTD, L_PTP,   L_BRD,   L_PTN,   _______, _______, _______, _______,  _______,  KC_LEFT, KC_DOWN, KC_RGHT,          _______, KC_VOLD, \
+        _______, L_T_MD,  L_T_ONF, _______, _______, _______, TG_NKRO, _______,  _______,  _______, _______, _______,          KC_PGUP, KC_MPLY, \
+        _______, _______, _______,                            _______,                              _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     /*
     [X] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
     */
 };
@@ -132,7 +133,7 @@ void set_sleep_state(bool keep_sleeping) {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-  led_lighting_mode = LED_MODE_KEYS_ONLY;
+  led_lighting_mode = LED_MODE_DEFAULT;
   gcr_desired       = LED_GCR_DEFAULT;
   led_animation_id  = LED_ANIMATION_ID_DEFAULT;
 };
